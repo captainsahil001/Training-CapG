f43e0884f94cc91c4091cc2670c1a2c4a8ad4125
captainsahil001 <sahilghosh0512@gmail.com>
2026-01-29 10:15:07 +0530

    added gp

--- DSA/src/timeandspaceComp/Gp.java
+++ DSA/src/timeandspaceComp/Gp.java
@@ -1,5 +1,18 @@
 package timeandspaceComp;
 
 public class Gp {
-	
+	public static void printGP(int a, int r, int n) {
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            a = a * r;
+        }
+        System.out.println();
+    }
+	public static void main(String[] args) {
+		int a = 3;
+		int r = 2;
+		int n = 5;
+		printGP(a,r,n);
+	}
 }
